094a355619b32801ccdd4aee4e3a5794cb965565
Yoinkbird <yoinkbird2010@gmail.com>
2014-11-01 16:21:26 -0500

    HACK - initial json request - part10
    adapted from http://www.vogella.com/tutorials/AndroidJSON/article.html
    This is not a very good tutorial but the json request is working so I should keep moving and work on the actual features
    
    part10:
    renamed DisplayMessageActivity to SearchStreamsActivity
    
    part9:
    SearchStreamsActivity: adding self-submitting intent so that search queries return to page
    MainActivity: renaming button function
    
    part8:
    updated layout
    
    part7:
    adding json functionality to DisplayMessageActivity, which is to become the SearchStreamsActivity
    
    part6:
    pass in json request url to functions instead of hard-coding
    
    part5:
    refactor json activities into functions
    (still a terrible implementation)
    
    part4:
    removing extraneous code

--- src/com/example/myfirstapp/SearchStreamsActivity.java
+++ src/com/example/myfirstapp/SearchStreamsActivity.java
@@ -14,35 +14,35 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 
-import android.app.Activity;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
-	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+public class SearchStreamsActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
 		
-		String searchTerm = "test";
+		// Get the message from the intent
+		Intent intent = getIntent();
+		String searchTerm = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 		String jsonRequestURL = "http://fizzenglorp.appspot.com/genericquery?redirect=0&term=" + searchTerm;
-
+		
 		String jsonReturnString = getJsonString(jsonRequestURL);
+		// Set the text view as the activity layout
 		if(true){
 			// Set the text view as the activity layout
-			setContentView(R.layout.fragment_main);
+			setContentView(R.layout.fragment_display_message);
 			// get text view for json
 			TextView jsonTextView = (TextView) findViewById(R.id.display_json);
 			jsonTextView.setText(jsonReturnString);
@@ -50,13 +50,13 @@ public class MainActivity extends Activity {
 			//Log.i(MainActivity.class.getName(), "json Text: " + jsonReturnString);
 		}
 		else{
-			setContentView(R.layout.activity_main);
 			if (savedInstanceState == null) {
-				getFragmentManager().beginTransaction()
+				getSupportFragmentManager().beginTransaction()
 				.add(R.id.container, new PlaceholderFragment()).commit();
 			}
 		}
-
+		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 	public String getJsonString(String httpUrl) {
 		// Just for testing, allow network access in the main thread
@@ -72,7 +72,7 @@ public class MainActivity extends Activity {
 			//JSONObject jsonObject = new JSONObject(jsonReturnString);
 			JSONArray jsonObject = new JSONArray(jsonReturnString);
 			jsonReturnString = jsonObject.toString();
-			Log.i(MainActivity.class.getName(), jsonObject.toString());
+			Log.i(this.getClass().getSimpleName(), jsonObject.toString());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -106,30 +106,33 @@ public class MainActivity extends Activity {
 		}
 		return builder.toString();
 	}
-
+	/** Called when the user clicks the Search button */
+	public void searchQuery(View view){
+		Intent intent = new Intent(this, SearchStreamsActivity.class);
+		EditText editText = (EditText) findViewById(R.id.search_term);
+		String message = editText.getText().toString();
+		String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+	}
+/*
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main_activity_actions, menu);
-		//return true;
-		return super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.display_message, menu);
+		return true;
 	}
-
+*/
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		switch (item.getItemId()) {
-			case R.id.action_search:
-				//openSearch();
-				return true;
-			case R.id.action_settings:
-				//openSettings();
-				return true;
-			default:
-				return super.onOptionsItemSelected(item);
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
 		}
+		return super.onOptionsItemSelected(item);
 	}
 
 	/**
@@ -143,33 +146,9 @@ public class MainActivity extends Activity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
+			View rootView = inflater.inflate(R.layout.fragment_display_message,
+					container, false);
 			return rootView;
-		}
-	}
-	
-	/** Called when the user clicks the Send button */
-	public void sendSearchQuery(View view){
-		Intent intent = new Intent(this, SearchStreamsActivity.class);
-		EditText editText = (EditText) findViewById(R.id.edit_message);
-		String message = editText.getText().toString();
-		intent.putExtra(EXTRA_MESSAGE, message);
-		startActivity(intent);
-	}
-
-	/** Called when the user clicks the 'Stop App' button */
-	/** see also http://stackoverflow.com/a/4153842
-	 * */
-	public void stopApplication(View view){
-		//POC: show that this function is being called by updating the text input field
-		if(false){
-			EditText editText = (EditText) findViewById(R.id.edit_message);
-			editText.setText("defaultvalue");
-		}
-		// end program lifecycle 
-		else{
-			finish();
 		}
 	}
 }
