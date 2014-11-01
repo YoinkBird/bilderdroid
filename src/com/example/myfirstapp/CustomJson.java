69292e7e420912813ffa9ac2b41610e51fbeb332
Yoinkbird <yoinkbird2010@gmail.com>
2014-11-01 16:21:26 -0500

    HACK - initial json request - part15
    adapted from http://www.vogella.com/tutorials/AndroidJSON/article.html
    This is not a very good tutorial but the json request is working so I should keep moving and work on the actual features
    
    part15:
    updating hacky JSON functions - adding support for JSONObject or JSONArray
    
    part14:
    updating hacky JSON functions - moving the de-threading into the http request; does not belong in json request
    
    part13:
    moved hacky JSON functions into separate Class (and therefore file)
    
    part12:
    updated MainActivity layout to remove all traces of json
    removed unneded imports
    
    part11:
    removed json functionality from MainActivity
    
    part10a:
    renamed DisplayMessageActivity to SearchStreamsActivity
    
    part10:
    renamed DisplayMessageActivity to SearchStreamsActivity
    
    part9:
    SearchStreamsActivity: adding self-submitting intent so that search queries return to page
    MainActivity: renaming button function
    
    part8:
    updated layout
    
    part7:
    adding json functionality to DisplayMessageActivity, which is to become the SearchStreamsActivity
    
    part6:
    pass in json request url to functions instead of hard-coding
    
    part5:
    refactor json activities into functions
    (still a terrible implementation)
    
    part4:
    removing extraneous code

--- src/com/example/myfirstapp/CustomJson.java
+++ src/com/example/myfirstapp/CustomJson.java
@@ -11,8 +11,10 @@ import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import android.os.StrictMode;
 import android.util.Log;
@@ -27,15 +29,35 @@ public class CustomJson {
 	public String getJsonString(String httpUrl) {
 		//setContentView(R.layout.main);
 		String jsonReturnString = sendHttpRequest(httpUrl);
+		return jsonReturnString;
+	}
+	public JSONObject getJsonObject(String httpUrl) {
+		JSONObject jsonObject = null;
+		//setContentView(R.layout.main);
+		String jsonReturnString = sendHttpRequest(httpUrl);
 		try {
 			//JSONObject jsonObject = new JSONObject(jsonReturnString);
-			JSONArray jsonObject = new JSONArray(jsonReturnString);
+			jsonObject = new JSONObject(jsonReturnString);
 			jsonReturnString = jsonObject.toString();
 			Log.i(this.getClass().getSimpleName(), jsonObject.toString());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return jsonReturnString;
+		return jsonObject;
+	}
+	public JSONArray getJsonArray(String httpUrl) {
+		JSONArray jsonArray = null;
+		//setContentView(R.layout.main);
+		String jsonReturnString = sendHttpRequest(httpUrl);
+		try {
+			//JSONObject jsonObject = new JSONObject(jsonReturnString);
+			jsonArray = new JSONArray(jsonReturnString);
+			jsonReturnString = jsonArray.toString();
+			Log.i(this.getClass().getSimpleName(), jsonArray.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return jsonArray;
 	}
 	public String sendHttpRequest(String httpUrl) {
 		// Just for testing, allow network access in the main thread
@@ -49,8 +71,10 @@ public class CustomJson {
 		//String httpUrl = "https://bugzilla.mozilla.org/rest/bug?assigned_to=lhenry@mozilla.com";
 		//httpUrl = "http://fizzenglorp.appspot.com/genericquery?redirect=0&term=test";
 		HttpGet httpGet = new HttpGet(httpUrl);
+		HttpPost httpPost = new HttpPost(httpUrl);
 		try {
-			HttpResponse response = client.execute(httpGet);
+//			HttpResponse response = client.execute(httpGet);
+			HttpResponse response = client.execute(httpPost);
 			StatusLine statusLine = response.getStatusLine();
 			int statusCode = statusLine.getStatusCode();
 			if (statusCode == 200) {
