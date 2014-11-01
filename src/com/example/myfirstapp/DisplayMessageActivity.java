67b25e777c30ee62276750f41b1618d38e46a102
Yoinkbird <yoinkbird2010@gmail.com>
2014-11-01 16:21:26 -0500

    HACK - initial json request - part7
    adapted from http://www.vogella.com/tutorials/AndroidJSON/article.html
    This is not a very good tutorial but the json request is working so I should keep moving and work on the actual features
    
    part7:
    adding json functionality to DisplayMessageActivity, which is to become the SearchStreamsActivity
    
    part6:
    pass in json request url to functions instead of hard-coding
    
    part5:
    refactor json activities into functions
    (still a terrible implementation)
    
    part4:
    removing extraneous code

--- src/com/example/myfirstapp/DisplayMessageActivity.java
+++ src/com/example/myfirstapp/DisplayMessageActivity.java
@@ -1,9 +1,25 @@
 package com.example.myfirstapp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,21 +34,76 @@ public class DisplayMessageActivity extends ActionBarActivity {
 		
 		// Get the message from the intent
 		Intent intent = getIntent();
-		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+		String searchTerm = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+		String jsonRequestURL = "http://fizzenglorp.appspot.com/genericquery?redirect=0&term=" + searchTerm;
 		
-		// Create the text view
-		TextView textView = new TextView(this);
-		textView.setTextSize(40);
-		textView.setText(message);
-		
+		String jsonReturnString = getJsonString(jsonRequestURL);
 		// Set the text view as the activity layout
-		setContentView(R.layout.activity_display_message);
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+		if(true){
+			// Set the text view as the activity layout
+			setContentView(R.layout.fragment_display_message);
+			// get text view for json
+			TextView jsonTextView = (TextView) findViewById(R.id.display_json);
+			jsonTextView.setText(jsonReturnString);
+			//Log.i(MainActivity.class.getName(), "jsonTextView: " + jsonTextView.toString());
+			//Log.i(MainActivity.class.getName(), "json Text: " + jsonReturnString);
+		}
+		else{
+			if (savedInstanceState == null) {
+				getSupportFragmentManager().beginTransaction()
+				.add(R.id.container, new PlaceholderFragment()).commit();
+			}
 		}
 		
 		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+	public String getJsonString(String httpUrl) {
+		// Just for testing, allow network access in the main thread
+		// NEVER use this is productive code
+		StrictMode.ThreadPolicy policy = new StrictMode.
+				ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy); 
+
+
+		//setContentView(R.layout.main);
+		String jsonReturnString = sendHttpRequest(httpUrl);
+		try {
+			//JSONObject jsonObject = new JSONObject(jsonReturnString);
+			JSONArray jsonObject = new JSONArray(jsonReturnString);
+			jsonReturnString = jsonObject.toString();
+			Log.i(this.getClass().getSimpleName(), jsonObject.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return jsonReturnString;
+	}
+	public String sendHttpRequest(String httpUrl) {
+		StringBuilder builder = new StringBuilder();
+		HttpClient client = new DefaultHttpClient();
+		//String httpUrl = "https://bugzilla.mozilla.org/rest/bug?assigned_to=lhenry@mozilla.com";
+		//httpUrl = "http://fizzenglorp.appspot.com/genericquery?redirect=0&term=test";
+		HttpGet httpGet = new HttpGet(httpUrl);
+		try {
+			HttpResponse response = client.execute(httpGet);
+			StatusLine statusLine = response.getStatusLine();
+			int statusCode = statusLine.getStatusCode();
+			if (statusCode == 200) {
+				HttpEntity entity = response.getEntity();
+				InputStream content = entity.getContent();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					builder.append(line);
+				}
+			} else {
+				//Log.e(ParseJSON.class.toString(), "Failed to download file");
+			}
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return builder.toString();
 	}
 /*
 	@Override
