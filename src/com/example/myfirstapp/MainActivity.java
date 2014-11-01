c89a82e26b1c597491bad7d720100579ea375f09
Yoinkbird <yoinkbird2010@gmail.com>
2014-11-01 16:21:26 -0500

    HACK - initial json request - part11
    adapted from http://www.vogella.com/tutorials/AndroidJSON/article.html
    This is not a very good tutorial but the json request is working so I should keep moving and work on the actual features
    
    part11:
    removed json functionality from MainActivity
    
    part10a:
    renamed DisplayMessageActivity to SearchStreamsActivity
    
    part10:
    renamed DisplayMessageActivity to SearchStreamsActivity
    
    part9:
    SearchStreamsActivity: adding self-submitting intent so that search queries return to page
    MainActivity: renaming button function
    
    part8:
    updated layout
    
    part7:
    adding json functionality to DisplayMessageActivity, which is to become the SearchStreamsActivity
    
    part6:
    pass in json request url to functions instead of hard-coding
    
    part5:
    refactor json activities into functions
    (still a terrible implementation)
    
    part4:
    removing extraneous code

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -35,19 +35,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		
-		String searchTerm = "test";
-		String jsonRequestURL = "http://fizzenglorp.appspot.com/genericquery?redirect=0&term=" + searchTerm;
-
-		String jsonReturnString = getJsonString(jsonRequestURL);
 		if(true){
 			// Set the text view as the activity layout
 			setContentView(R.layout.fragment_main);
-			// get text view for json
-			TextView jsonTextView = (TextView) findViewById(R.id.display_json);
-			jsonTextView.setText(jsonReturnString);
-			//Log.i(MainActivity.class.getName(), "jsonTextView: " + jsonTextView.toString());
-			//Log.i(MainActivity.class.getName(), "json Text: " + jsonReturnString);
 		}
 		else{
 			setContentView(R.layout.activity_main);
@@ -57,54 +47,6 @@ public class MainActivity extends Activity {
 			}
 		}
 
-	}
-	public String getJsonString(String httpUrl) {
-		// Just for testing, allow network access in the main thread
-		// NEVER use this is productive code
-		StrictMode.ThreadPolicy policy = new StrictMode.
-				ThreadPolicy.Builder().permitAll().build();
-		StrictMode.setThreadPolicy(policy); 
-
-
-		//setContentView(R.layout.main);
-		String jsonReturnString = sendHttpRequest(httpUrl);
-		try {
-			//JSONObject jsonObject = new JSONObject(jsonReturnString);
-			JSONArray jsonObject = new JSONArray(jsonReturnString);
-			jsonReturnString = jsonObject.toString();
-			Log.i(MainActivity.class.getName(), jsonObject.toString());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return jsonReturnString;
-	}
-	public String sendHttpRequest(String httpUrl) {
-		StringBuilder builder = new StringBuilder();
-		HttpClient client = new DefaultHttpClient();
-		//String httpUrl = "https://bugzilla.mozilla.org/rest/bug?assigned_to=lhenry@mozilla.com";
-		//httpUrl = "http://fizzenglorp.appspot.com/genericquery?redirect=0&term=test";
-		HttpGet httpGet = new HttpGet(httpUrl);
-		try {
-			HttpResponse response = client.execute(httpGet);
-			StatusLine statusLine = response.getStatusLine();
-			int statusCode = statusLine.getStatusCode();
-			if (statusCode == 200) {
-				HttpEntity entity = response.getEntity();
-				InputStream content = entity.getContent();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(content));
-				String line;
-				while ((line = reader.readLine()) != null) {
-					builder.append(line);
-				}
-			} else {
-				//Log.e(ParseJSON.class.toString(), "Failed to download file");
-			}
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return builder.toString();
 	}
 
 	@Override
