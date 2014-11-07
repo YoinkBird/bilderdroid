6d357021de6648db73c0b5232b1cb758f7c4e70a
Yoinkbird <yoinkbird2010@gmail.com>
2014-11-07 11:26:51 -0600

    initial intent switching logic working
    this allows different json to be retrieved based on which button is selected

--- src/com/example/myfirstapp/ViewAllStreamsActivity.java
+++ src/com/example/myfirstapp/ViewAllStreamsActivity.java
@@ -17,6 +17,7 @@ import android.widget.GridView;
 import android.widget.Toast;
 
 public class ViewAllStreamsActivity extends Activity {
+	public final static String DISPLAY_STREAM_SELECTOR = "com.example.myfirstapp.DISPLAY_STREAM_SELECTOR";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +25,64 @@ public class ViewAllStreamsActivity extends Activity {
 		setContentView(R.layout.fragment_view_all_streams);
 		setupButtonListeners();
 
-		String[] mTestThumbUrls;
-		String searchTerm = "android";
-		String jsonRequestURL = "http://fizzenglorp.appspot.com/genericquery?redirect=0&term=" + searchTerm;
+		// parse intent to determine what to do
+		/* Either:
+		 * * show latest streams (default)
+		 * * show latest subscribed streams (button)
+		 * * show nearby streams (button)
+		 */
+		String searchTerm = null;
+		String jsonRequestURL = null;
+		String displayStreams = "all";
+		Intent intent = getIntent();
+		//TODO: remove hardcode for testing
+		searchTerm = "android";
+//		searchTerm = intent.getStringExtra(ViewAllStreamsActivity.DISPLAY_STREAM_SELECTOR);
+		displayStreams = intent.getStringExtra(ViewAllStreamsActivity.DISPLAY_STREAM_SELECTOR);
+		if(displayStreams == null){
+			displayStreams = "all";
+		}
+		jsonRequestURL = "http://fizzenglorp.appspot.com/genericquery?redirect=0&term=" + searchTerm;
+		Log.i(this.getClass().getSimpleName(),  "DISPLAY_STREAM_SELECTOR - searchTerm: " + searchTerm);
+		Log.i(this.getClass().getSimpleName(),  "DISPLAY_STREAM_SELECTOR - displayStreams: " + displayStreams);
+
+		String[] mTestThumbUrls = null;
 		//TODO: create simple URL to retrieve all images for a stream, all covers for all streams, etc
-		if(true){
+//		if(displayStreams == null){// || displayStreams.equals("all") || displayStreams == null){
+//		if(displayStreams == "all" || displayStreams == null){
+		if(displayStreams.equals("all")){// == "subscribed"){
+			searchTerm = "android";
 			//json format: [{"streamid":"<streamid>","coverurl":"http://<url>"},]
-			jsonRequestURL = "http://fizzenglorp.appspot.com/viewallstreams?redirect=0&term=" + searchTerm;
-			Log.i(this.getClass().getSimpleName(),  "jsonRequestURL: " + jsonRequestURL.toString());
-//			JSONArray jsonImgArray = getJsonArray(jsonRequestURL);
+			jsonRequestURL = "http://fizzenglorp.appspot.com/viewallstreams?redirect=0";
+//			Log.i(this.getClass().getSimpleName(),  "jsonRequestURL for all: " + jsonRequestURL.toString());
+			Log.i(this.getClass().getSimpleName(),  "jsonRequestURL for " + displayStreams + ": " + jsonRequestURL.toString());
+			// Get the JSON Array or Object
 			CustomJson customJsonObj = new CustomJson();
 			JSONArray jsonImgArray = customJsonObj.getJsonArray(jsonRequestURL);
+			// Get data from the JSON Array or Object
+			CustomJsonBilderapp customJsonBilderappObj = new CustomJsonBilderapp();
+			mTestThumbUrls = customJsonBilderappObj.parseJsonFromViewAllStreams(jsonImgArray);
+		}
+		if(displayStreams == "nearby"){
+			searchTerm = "grass";
+			//TODO: determine the json format: [{"streamid":"<streamid>","coverurl":"http://<url>"},]
+			jsonRequestURL = "http://fizzenglorp.appspot.com/viewallstreams?redirect=0&term=" + searchTerm;
+			Log.i(this.getClass().getSimpleName(),  "jsonRequestURL for " + displayStreams + ": " + jsonRequestURL.toString());
+			// Get the JSON Array or Object
+			CustomJson customJsonObj = new CustomJson();
+			JSONArray jsonImgArray = customJsonObj.getJsonArray(jsonRequestURL);
+			// Get data from the JSON Array or Object
+			CustomJsonBilderapp customJsonBilderappObj = new CustomJsonBilderapp();
+			mTestThumbUrls = customJsonBilderappObj.parseJsonFromViewAllStreams(jsonImgArray);
+		}
+		if(displayStreams.equals("subscribed")){// == "subscribed"){
+			//json format: [{"streamid":"<streamid>","coverurl":"http://<url>"},]
+			jsonRequestURL = "http://fizzenglorp.appspot.com/viewallstreams?redirect=0&term=" + searchTerm;
+			Log.i(this.getClass().getSimpleName(),  "jsonRequestURL for " + displayStreams + ": " + jsonRequestURL.toString());
+			// Get the JSON Array or Object
+			CustomJson customJsonObj = new CustomJson();
+			JSONArray jsonImgArray = customJsonObj.getJsonArray(jsonRequestURL);
+			// Get data from the JSON Array or Object
 			CustomJsonBilderapp customJsonBilderappObj = new CustomJsonBilderapp();
 			mTestThumbUrls = customJsonBilderappObj.parseJsonFromViewAllStreams(jsonImgArray);
 		}
@@ -82,6 +130,13 @@ public class ViewAllStreamsActivity extends Activity {
 		intent.putExtra(MainActivity.EXTRA_MESSAGE, message);
 		startActivity(intent);
 	}
+	/** Called when the user clicks one of the buttons for viewing streams*/
+	public void startViewAllStreamsActivity(View view, String streamType){
+		Intent intent = new Intent(this, ViewAllStreamsActivity.class);
+		intent.putExtra(DISPLAY_STREAM_SELECTOR, streamType);
+		startActivity(intent);
+	}
+
    private void setupButtonListeners(){
 	 // listen to the button for 'search'
      Button searchResultsButton = (Button) findViewById(R.id.button_search_streams);
@@ -91,6 +146,28 @@ public class ViewAllStreamsActivity extends Activity {
            public void onClick(View v) {
         	   //TODO
         	   sendSearchQuery(v);
+           }
+         }
+         );
+	 // listen to the button for 'nearby'
+     Button viewNearbyStreamsButton = (Button) findViewById(R.id.button_find_nearby_streams);
+     viewNearbyStreamsButton.setOnClickListener(
+         new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+        	   //TODO
+        	   startViewAllStreamsActivity(v, "nearby");
+           }
+         }
+         );
+	 // listen to the button for 'subscribed streams'
+     Button viewSubscribedStreamsButton = (Button) findViewById(R.id.button_subscribed_streams);
+     viewSubscribedStreamsButton.setOnClickListener(
+         new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+        	   //TODO
+        	   startViewAllStreamsActivity(v, "subscribed");
            }
          }
          );
