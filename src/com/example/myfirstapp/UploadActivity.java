9388d1dfa9e4d3a8c2e3e706d591737b6cbcf266
Yoinkbird <yoinkbird2010@gmail.com>
2014-11-07 22:32:20 -0600

    refactor for uploader - removing unneded libs

--- src/com/example/myfirstapp/UploadActivity.java
+++ src/com/example/myfirstapp/UploadActivity.java
@@ -7,16 +7,11 @@ import java.io.IOException;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.hardware.Camera;
-import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 //source: http://www.tutorialspoint.com/android/android_camera.htm
